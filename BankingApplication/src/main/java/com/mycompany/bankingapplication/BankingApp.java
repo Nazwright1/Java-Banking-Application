5cfb03d5cc18da212dccbd4c6ba529babfb98323
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-06-22 01:48:19 -0400

    show Menu method update
    added options for checking balance, depositing money, withdrawing money, viewing previous transaction info.

--- BankingApplication/src/main/java/com/mycompany/bankingapplication/BankingApp.java
+++ BankingApplication/src/main/java/com/mycompany/bankingapplication/BankingApp.java
@@ -34,14 +34,26 @@ class BankAccount {
     String customerName; 
     
     String customerId; 
+    /*
+    *
+    */
+    BankAccount(String cName, String cId){ 
+        customerName = cName; 
+        customerId = cId; 
+    }
     
-    
+    /*
+    *
+    */
     void deposit(int amount) { 
         
         if (amount != 0) { 
             balance = amount + balance ; 
         }
     }
+    /* 
+    *
+    */
     void withdraw (int amount) { 
         if(amount !=0) {
             balance = balance - amount; 
@@ -52,6 +64,9 @@ class BankAccount {
         
        
     }
+    /*
+    *
+    */
     void getPreviousTransaction(){ 
         if(previousTransaction > 0) { 
             System.out.println("Deposited: " +previousTransaction); 
@@ -62,5 +77,79 @@ class BankAccount {
         else { 
             System.out.println("No transactions found.");
         }
+        
+        
     }
+    /*
+    * 
+    */
+   
+    void showMenu() { 
+        
+        //represents the menu option selected by the user.
+        char option = '\0';
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Welcome " +customerName); 
+        System.out.println("Your ID is " + customerId);
+        System.out.println("\n");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous Transaction");
+        System.out.println("E. Exit App");
+        
+        do { 
+        System.out.println("==================================================================");
+        System.out.println("Enter An Option");
+        System.out.println("==================================================================");
+        option = scan.next().charAt(0);
+        System.out.println("/n"); 
+        
+        switch(option)
+        { 
+            case 'A': 
+                System.out.println("========================");
+               System.out.println("Balance = " +balance); 
+                System.out.println("========================");
+                System.out.println("/n");
+        
+           case 'B': 
+                System.out.println("========================");
+               System.out.println("Enter the amount you would like to deposit."); 
+               int amount = scan.nextInt();
+               deposit(amount); 
+               System.out.println(amount+ " has been deposited.");
+                System.out.println("========================");
+                System.out.println("/n");
+                
+           case 'C': 
+                System.out.println("========================");
+               System.out.println("Enter the amount you would like to withdraw.");
+               amount = scan.nextInt(); 
+               withdraw(amount); 
+               System.out.println(amount+" has been withdrawn.");
+                System.out.println("========================");
+                System.out.println("/n");
+               
+           case 'D': 
+                System.out.println("========================");
+               getPreviousTransaction();
+                System.out.println("========================");
+                 System.out.println("/n");
+           default:
+               System.out.print("Invalid Option! Please enter again");
+        
+        }
+        
+        } 
+        while(option != 'E'); 
+      
+        System.out.println("Thank you for using our services.");
+        
+    
+        }
 }
+
+
+
