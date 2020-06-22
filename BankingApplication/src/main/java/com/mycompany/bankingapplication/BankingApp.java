1916a8588e2fc23c677ce112e76039526a97abf9
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-06-21 23:41:42 -0400

    User input testing
    added scanner class to test user input

---
+++ BankingApplication/src/main/java/com/mycompany/bankingapplication/BankingApp.java
@@ -0,0 +1,28 @@
+package com.mycompany.bankingapplication;
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nazwright
+ */
+public class BankingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //define scanner 
+        Scanner scanner = new Scanner(System.in); 
+        int a = scanner.nextInt(); 
+        System.out.println("value = " + a); 
+    }
+    
+}
